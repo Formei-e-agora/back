15e4df685b97b050402f0e777b11c6bcce4a181e
José Marcel Mendes Nogueira <marcelnogueirasgs@gmail.com>
2020-09-16 16:56:21 -0300

    feat: person routes

---
+++ person-microservice/src/api/routes/person.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const personServices = require('../controllers/personServices');
+
+const route = express.Router();
+
+module.exports = (server) => {
+  server.use('', route);
+
+  route.get('/verify/personId/:personId', personServices.verifyPersonExists);
+  route.get('/find/all', personServices.findAll);
+  route.get('/find/personId/:personId', personServices.findByPk);
+  route.post('/create', personServices.create);
+  route.put('/update/:personId', personServices.update);
+  route.delete('/delete/:personId', personServices.delete);
+};

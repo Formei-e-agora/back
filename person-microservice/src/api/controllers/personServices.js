3e227e26f35b99e8ec5196f443caef042b8ae5d3
José Marcel Mendes Nogueira <marcelnogueirasgs@gmail.com>
2020-09-23 20:49:52 -0300

    fix: update

--- person-microservice/src/api/controllers/personServices.js
+++ person-microservice/src/api/controllers/personServices.js
@@ -1,101 +1,101 @@
-const { Person } = require('../../models');
-const generalDao = require('../../database/dao/generalDao');
-const errors = require('../../../../helpers/errors/errorCodes');
-const asyncHandler = require('../../../../helpers/handlers/asyncHandler');
-const ErrorResponse = require('../../../../helpers/errors/ErrorResponse');
+const { Person } = require('../../models')
+const generalDao = require('../../database/dao/generalDao')
+const errors = require('../../../../helpers/errors/errorCodes')
+const asyncHandler = require('../../../../helpers/handlers/asyncHandler')
+const ErrorResponse = require('../../../../helpers/errors/ErrorResponse')
 
 exports.create = asyncHandler(async (req, res) => {
-  const result = await generalDao.create(Person, req.body);
+  const result = await generalDao.create(Person, req.body)
 
   if (!result) {
-    throw new ErrorResponse(errors.COULD_NOT_CREATE, result);
+    throw new ErrorResponse(errors.COULD_NOT_CREATE, result)
   }
   if (result.name === 'SequelizeUniqueConstraintError') {
-    throw new ErrorResponse(errors.COULD_NOT_CREATE, result);
+    throw new ErrorResponse(errors.COULD_NOT_CREATE, result)
   }
   if (result.name === 'SequelizeForeignKeyConstraintError') {
-    throw new ErrorResponse(errors.COULD_NOT_CREATE, result);
+    throw new ErrorResponse(errors.COULD_NOT_CREATE, result)
   }
   if (result.name === 'SequelizeDatabaseError') {
-    throw new ErrorResponse(errors.COULD_NOT_CREATE, result);
+    throw new ErrorResponse(errors.COULD_NOT_CREATE, result)
   }
 
-  return res.json({ Status: true, personData: result }).end();
-});
+  return res.json({ Status: true, personData: result }).end()
+})
 
 exports.update = asyncHandler(async (req, res) => {
   const result = await generalDao.update(Person, req.body, {
-    personId: req.params.personId,
-  });
+    personId: req.params.personId
+  })
 
   if (!result) {
-    throw new ErrorResponse(errors.COULD_NOT_UPDATE, result);
+    throw new ErrorResponse(errors.COULD_NOT_UPDATE, result)
   }
   if (result.name === 'SequelizeUniqueConstraintError') {
-    throw new ErrorResponse(errors.COULD_NOT_UPDATE, result);
+    throw new ErrorResponse(errors.COULD_NOT_UPDATE, result)
   }
   if (result.name === 'SequelizeForeignKeyConstraintError') {
-    throw new ErrorResponse(errors.COULD_NOT_UPDATE, result);
+    throw new ErrorResponse(errors.COULD_NOT_UPDATE, result)
   }
   if (result.name === 'SequelizeDatabaseError') {
-    throw new ErrorResponse(errors.COULD_NOT_UPDATE, result);
+    throw new ErrorResponse(errors.COULD_NOT_UPDATE, result)
   }
   if (result.length === 0) {
-    throw new ErrorResponse(errors.NOT_FOUND, result);
+    throw new ErrorResponse(errors.NOT_FOUND, result)
   }
 
-  return res.json({ Status: true, personData: result }).end();
-});
+  return res.json({ Status: true, personData: result[0] }).end()
+})
 
 exports.delete = asyncHandler(async (req, res) => {
   const result = await generalDao.delete(Person, {
-    personId: req.params.personId,
-  });
+    personId: req.params.personId
+  })
 
   if (!result) {
-    throw new ErrorResponse(errors.COULD_NOT_DELETE, result);
+    throw new ErrorResponse(errors.COULD_NOT_DELETE, result)
   }
   if (result.name === 'SequelizeForeignKeyConstraintError') {
-    throw new ErrorResponse(errors.COULD_NOT_DELETE, result);
+    throw new ErrorResponse(errors.COULD_NOT_DELETE, result)
   }
   if (result.name === 'SequelizeDatabaseError') {
-    throw new ErrorResponse(errors.COULD_NOT_DELETE, result);
+    throw new ErrorResponse(errors.COULD_NOT_DELETE, result)
   }
   if (result === 0) {
-    throw new ErrorResponse(errors.NOT_FOUND, result);
+    throw new ErrorResponse(errors.NOT_FOUND, result)
   }
 
-  return res.json({ Status: true }).end();
-});
+  return res.json({ Status: true }).end()
+})
 
 exports.findByPk = asyncHandler(async (req, res) => {
-  const result = await generalDao.findByPk(Person, req.params.personId);
+  const result = await generalDao.findByPk(Person, req.params.personId)
 
   if (!result) {
-    throw new ErrorResponse(errors.NOT_FOUND, result);
+    throw new ErrorResponse(errors.NOT_FOUND, result)
   }
 
-  return res.json({ Status: true, personData: result }).end();
-});
+  return res.json({ Status: true, personData: result }).end()
+})
 
 exports.findAll = asyncHandler(async (req, res) => {
-  const result = await generalDao.findAll(Person);
+  const result = await generalDao.findAll(Person)
 
   if (!result) {
-    throw new ErrorResponse(errors.NOT_FOUND, result);
+    throw new ErrorResponse(errors.NOT_FOUND, result)
   }
   if (!result.length) {
-    throw new ErrorResponse(errors.NOT_FOUND, result);
+    throw new ErrorResponse(errors.NOT_FOUND, result)
   }
 
-  return res.json({ Status: true, persons: result }).end();
-});
+  return res.json({ Status: true, people: result }).end()
+})
 
 exports.verifyPersonExists = asyncHandler(async (req, res) => {
-  const result = await generalDao.verifyIfExistsById(Person, req.params.personId);
+  const result = await generalDao.verifyIfExistsById(Person, req.params.personId)
 
   if (result) {
-    return res.json({ Status: true }).end();
+    return res.json({ Status: true }).end()
   }
-  return res.json({ Status: false }).end();
-});
+  return res.json({ Status: false }).end()
+})

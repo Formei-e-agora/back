78ff2269ccfcd627a709db5a64a03ae165f57eab
José Marcel Mendes Nogueira <marcelnogueirasgs@gmail.com>
2020-09-23 20:30:00 -0300

    fix: jest config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,15 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
+  roots: ['person-microservice'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.js'
     // '!<rootDir>/src/main/**',
     // '!<rootDir>/src/**/*-protocols.js',
     // '!**/protocols/**',
     // '!**/test/**'
+  ],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/helpers/'
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node'

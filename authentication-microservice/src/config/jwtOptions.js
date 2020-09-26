21c90505d217b422d797f701004d2c7c7887b9ed
José Marcel Mendes Nogueira <marcelnogueirasgs@gmail.com>
2020-09-25 23:00:10 -0300

    feat: auth microservice config

---
+++ authentication-microservice/src/config/jwtOptions.js
@@ -0,0 +1,9 @@
+const { ExtractJwt } = require('passport-jwt');
+require('dotenv').config({ path: `${__dirname}/../.env` });
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.secretOrKey,
+};
+
+module.exports = jwtOptions;

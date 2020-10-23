9f06a2aef0d2d8b6beb960614a6fa9c455fba508
José Marcel Mendes Nogueira <marcelnogueirasgs@gmail.com>
2020-10-23 12:45:11 -0300

    feat: send newest jobs email

--- helpers/email/sendEmail.js
+++ helpers/email/sendEmail.js
@@ -43,6 +43,41 @@ function sendPass(userEmail, generatedPassword, name) {
   transport.close();
 }
 
+function sendNewestJobs(userEmail, jobs, name) {
+  const fromEmailAddress = mailAccountUser;
+  const toEmailAddress = userEmail;
+
+  const mailSubject = 'Recuperação de Senha';
+  let mailBody = fs.readFileSync(`${templatePath}/resetPass.html`, { encoding: 'utf-8', flag: 'r' });
+  mailBody = mailBody.replace('#NOME_DO_CONTATO', name);
+  mailBody = mailBody.replace('#NOVA_SENHA', jobs[0].description);
+
+  const transport = nodemailer.createTransport({
+    service: 'gmail',
+    tls: { rejectUnauthorized: false },
+    auth: {
+      user: mailAccountUser,
+      pass: mailAccountPassword,
+    },
+  });
+
+  const mail = {
+    from: `Formei e Agora <${fromEmailAddress}>`,
+    to: toEmailAddress,
+    subject: mailSubject,
+    html: mailBody,
+  };
+
+  transport.sendMail(mail, (error/* , response */) => {
+    if (error) {
+      console.log(error);
+      throw new ErrorResponse(errors.FAILED_TO_SEND_EMAIL);
+    }
+  });
+  transport.close();
+}
+
 module.exports = {
   sendPass,
+  sendNewestJobs,
 };
